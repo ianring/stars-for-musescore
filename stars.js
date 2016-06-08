3fc8292faab12ef561a33a6d66acf14a69da175e
Ring, Ian <iring@netsuite.com>
2016-06-08 13:45:40 -0400

    no-op placeholder

---
+++ stars.js
@@ -0,0 +1,14 @@
+function init() {};
+function run() {};
+function close() {};
+var mscorePlugin = 
+{
+    majorVersion: 1,
+    minorVersion: 1,
+    init: init,
+    run: run,
+    onClose: close
+};
+
+mscorePlugin;
+
